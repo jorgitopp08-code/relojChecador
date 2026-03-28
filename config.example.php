12f5911db6678cb04c061a764dfe1fa5d547b21b
jorgitopp08-code <jorgitopp08@gmail.com>
2026-03-28 15:50:16 -0500

    8 cambio

--- config.example.php
+++ config.example.php
@@ -1,9 +1,4 @@
 <?php
-
-return [
-    'DB_HOST' => 'mysql-apijorge.alwaysdata.net',
-    'DB_PORT' => 3306,
-    'DB_NAME' => 'reloj_checador',
-    'DB_USER' => 'apijorge',
-    'DB_PASSWORD' => 'clase1234',
-];
+$conn = mysqli_connect("mysql-apijorge.alwaysdata.net", "apijorge", "clase1234", "apijorge_reloj_checador", 3306);
+if (!$conn) die("Error de conexión");
+?>
